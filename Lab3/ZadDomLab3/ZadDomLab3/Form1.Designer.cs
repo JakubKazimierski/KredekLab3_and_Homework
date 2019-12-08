0ce0c5cddb0489b37a05dc04d90910af367d5ad5
Jakub <j.m.kazimierski@gmail.com>
2019-12-08 20:47:38 +0100

    Log in mechanism looks way better now

--- Lab3/ZadDomLab3/ZadDomLab3/Form1.Designer.cs
+++ Lab3/ZadDomLab3/ZadDomLab3/Form1.Designer.cs
@@ -33,11 +33,18 @@
             this.TextBoxUserName = new System.Windows.Forms.TextBox();
             this.TextBoxUserPassword = new System.Windows.Forms.TextBox();
             this.ButtonLogIn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LabelWithTiltle = new System.Windows.Forms.Label();
+            this.LabelLogInTitle = new System.Windows.Forms.Label();
+            this.PanelWithLogIn = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.PanelWithLogIn.SuspendLayout();
             this.SuspendLayout();
             // 
             // LabelUserName
             // 
-            this.LabelUserName.Location = new System.Drawing.Point(463, 100);
+            this.LabelUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.09346F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.LabelUserName.Location = new System.Drawing.Point(20, 83);
             this.LabelUserName.Name = "LabelUserName";
             this.LabelUserName.Size = new System.Drawing.Size(100, 23);
             this.LabelUserName.TabIndex = 0;
@@ -46,7 +53,8 @@
             // 
             // LabelUserPassword
             // 
-            this.LabelUserPassword.Location = new System.Drawing.Point(463, 141);
+            this.LabelUserPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.09346F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.LabelUserPassword.Location = new System.Drawing.Point(20, 129);
             this.LabelUserPassword.Name = "LabelUserPassword";
             this.LabelUserPassword.Size = new System.Drawing.Size(100, 23);
             this.LabelUserPassword.TabIndex = 1;
@@ -55,42 +63,90 @@
             // 
             // TextBoxUserName
             // 
-            this.TextBoxUserName.Location = new System.Drawing.Point(569, 102);
+            this.TextBoxUserName.Location = new System.Drawing.Point(142, 83);
             this.TextBoxUserName.Name = "TextBoxUserName";
             this.TextBoxUserName.Size = new System.Drawing.Size(129, 20);
             this.TextBoxUserName.TabIndex = 2;
             // 
             // TextBoxUserPassword
             // 
-            this.TextBoxUserPassword.Location = new System.Drawing.Point(569, 141);
+            this.TextBoxUserPassword.Location = new System.Drawing.Point(142, 129);
             this.TextBoxUserPassword.Name = "TextBoxUserPassword";
             this.TextBoxUserPassword.Size = new System.Drawing.Size(129, 20);
             this.TextBoxUserPassword.TabIndex = 3;
             // 
             // ButtonLogIn
             // 
-            this.ButtonLogIn.Location = new System.Drawing.Point(567, 200);
+            this.ButtonLogIn.BackColor = System.Drawing.Color.LightSlateGray;
+            this.ButtonLogIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.747663F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ButtonLogIn.Location = new System.Drawing.Point(84, 181);
             this.ButtonLogIn.Name = "ButtonLogIn";
             this.ButtonLogIn.Size = new System.Drawing.Size(131, 36);
             this.ButtonLogIn.TabIndex = 4;
             this.ButtonLogIn.Text = "LogIn";
-            this.ButtonLogIn.UseVisualStyleBackColor = true;
+            this.ButtonLogIn.UseVisualStyleBackColor = false;
             this.ButtonLogIn.Click += new System.EventHandler(this.ButtonLogIn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ZadDomLab3.Properties.Resources.szkolaImage;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 268);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(832, 459);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LabelWithTiltle
+            // 
+            this.LabelWithTiltle.BackColor = System.Drawing.Color.Gray;
+            this.LabelWithTiltle.Font = new System.Drawing.Font("Impact", 22.20561F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelWithTiltle.Location = new System.Drawing.Point(34, 67);
+            this.LabelWithTiltle.Name = "LabelWithTiltle";
+            this.LabelWithTiltle.Size = new System.Drawing.Size(344, 97);
+            this.LabelWithTiltle.TabIndex = 6;
+            this.LabelWithTiltle.Text = "I Liceum Ogólnokształcące";
+            this.LabelWithTiltle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LabelLogInTitle
+            // 
+            this.LabelLogInTitle.Font = new System.Drawing.Font("Impact", 14.13084F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.LabelLogInTitle.Location = new System.Drawing.Point(51, 21);
+            this.LabelLogInTitle.Name = "LabelLogInTitle";
+            this.LabelLogInTitle.Size = new System.Drawing.Size(195, 59);
+            this.LabelLogInTitle.TabIndex = 7;
+            this.LabelLogInTitle.Text = "Zaloguj Się";
+            this.LabelLogInTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // PanelWithLogIn
+            // 
+            this.PanelWithLogIn.BackColor = System.Drawing.Color.Gray;
+            this.PanelWithLogIn.Controls.Add(this.LabelUserName);
+            this.PanelWithLogIn.Controls.Add(this.LabelLogInTitle);
+            this.PanelWithLogIn.Controls.Add(this.LabelUserPassword);
+            this.PanelWithLogIn.Controls.Add(this.ButtonLogIn);
+            this.PanelWithLogIn.Controls.Add(this.TextBoxUserPassword);
+            this.PanelWithLogIn.Controls.Add(this.TextBoxUserName);
+            this.PanelWithLogIn.Location = new System.Drawing.Point(471, 12);
+            this.PanelWithLogIn.Name = "PanelWithLogIn";
+            this.PanelWithLogIn.Size = new System.Drawing.Size(300, 237);
+            this.PanelWithLogIn.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.ButtonLogIn);
-            this.Controls.Add(this.TextBoxUserPassword);
-            this.Controls.Add(this.TextBoxUserName);
-            this.Controls.Add(this.LabelUserPassword);
-            this.Controls.Add(this.LabelUserName);
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(856, 739);
+            this.Controls.Add(this.PanelWithLogIn);
+            this.Controls.Add(this.LabelWithTiltle);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "SchoolPlatform";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.PanelWithLogIn.ResumeLayout(false);
+            this.PanelWithLogIn.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -101,6 +157,10 @@
         private System.Windows.Forms.TextBox TextBoxUserName;
         private System.Windows.Forms.TextBox TextBoxUserPassword;
         private System.Windows.Forms.Button ButtonLogIn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label LabelWithTiltle;
+        private System.Windows.Forms.Label LabelLogInTitle;
+        private System.Windows.Forms.Panel PanelWithLogIn;
     }
 }
 
