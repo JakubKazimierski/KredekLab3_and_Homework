f86ac9effa3d2e59c954d76e3ecdf70ec7aa2efc
Jakub <j.m.kazimierski@gmail.com>
2019-12-08 20:20:58 +0100

    Added grid viever after login, to check if data base connection work properly

--- Lab3/ZadDomLab3/ZadDomLab3/Form2.Designer.cs
+++ Lab3/ZadDomLab3/ZadDomLab3/Form2.Designer.cs
@@ -28,19 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.GriedVieverDataBase = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.GriedVieverDataBase)).BeginInit();
             this.SuspendLayout();
+            // 
+            // GriedVieverDataBase
+            // 
+            this.GriedVieverDataBase.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GriedVieverDataBase.Location = new System.Drawing.Point(12, 12);
+            this.GriedVieverDataBase.Name = "GriedVieverDataBase";
+            this.GriedVieverDataBase.RowHeadersWidth = 46;
+            this.GriedVieverDataBase.Size = new System.Drawing.Size(260, 426);
+            this.GriedVieverDataBase.TabIndex = 0;
             // 
             // AfterLogInForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GriedVieverDataBase);
             this.Name = "AfterLogInForm";
             this.Text = "SchoolPlatform";
+            ((System.ComponentModel.ISupportInitialize)(this.GriedVieverDataBase)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView GriedVieverDataBase;
     }
 }
--- Lab3/ZadDomLab3/ZadDomLab3/Form2.cs
+++ Lab3/ZadDomLab3/ZadDomLab3/Form2.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,26 @@ namespace ZadDomLab3
 {
     public partial class AfterLogInForm : Form
     {
+        private readonly Func<SqlConnection> dbConnectionWareHouse = () => new SqlConnection(ConfigurationManager.ConnectionStrings["Lab3"].ConnectionString);
         public AfterLogInForm()
         {
             InitializeComponent();
+            GetDataFromTable();
         }
+
+        public void GetDataFromTable()
+        {
+            using (var connection = dbConnectionWareHouse())
+            {
+                connection.Open();
+                using (SqlDataAdapter adapter = new SqlDataAdapter("Select * From Worker", connection))
+                {
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+                    GriedVieverDataBase.DataSource = table;
+                }
+            }
+        }
+
     }
 }
